d83a8538e079056a8db4753dccb7fdb68ce346bd
GustavoMSandoval <gmsandoval2005@gmail.com>
2024-10-07 09:43:39 -0300

    adding style

--- src/app/pages/movie-view/movie-view.component.ts
+++ src/app/pages/movie-view/movie-view.component.ts
@@ -3,9 +3,10 @@ import { environment } from '../../../environments/environment';
 import { MovieDetail } from '../../interfaces/movie-detail';
 import { MovieService } from '../../services/movie.service';
 import { ActivatedRoute } from '@angular/router';
-
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 const imgUrl = environment.imgURL;
+
 
 @Component({
   selector: 'app-movie-view',
@@ -20,8 +21,9 @@ export class MovieViewComponent implements OnInit {
   movie: MovieDetail | undefined;
   movieId: any;
   title: string = '';
+  starSvg: SafeHtml | undefined;
 
-  constructor(private activeRoute: ActivatedRoute) {
+  constructor(private activeRoute: ActivatedRoute,private sanitizer: DomSanitizer) {
     this.activeRoute.params.subscribe(
       res => {
         this.movieId = res["id"]
@@ -34,6 +36,12 @@ export class MovieViewComponent implements OnInit {
     )
 
     this.activeRoute.firstChild?.params.subscribe()
+
+    this.starSvg = this.sanitizer.bypassSecurityTrustHtml(`
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFF55">
+        <path d="m384-334 96-74 96 74-36-122 90-64H518l-38-124-38 124H330l90 64-36 122ZM233-120l93-304L80-600h304l96-320 96 320h304L634-424l93 304-247-188-247 188Zm247-369Z"/>
+      </svg>
+    `);
   }
 
  
